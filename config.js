348b49ea78ef48df40e0df9999d1f2352137f88f
sadeepanew <160372456+sadeepanew@users.noreply.github.com>
2024-02-21 00:11:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ global.ANTI_LINK = true //Anti Link (on = True , Off = False )
 
 global.ANTI_BOTS = true
 
-global.ONLY_GROUP = 'thue' // Only group mode
+global.ONLY_GROUP = 'true' // Only group mode
 
 global.GROUP_LINK = 'https://chat.whatsapp.com/IRdyJY5P4sF5BhtP5PIdHH' // ur group link
 
